6d099a1216cc9d415049694affdb6346833bb2a7
Nalinda Wickramarathna <mastersilicon@gmail.com>
2023-05-01 00:07:05 +0530

    Initial commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Nalinda Wickramarathna on 2023-04-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
