e73f190c7c7d2f8d77abe35c77fce1ce6f305424
Nalinda Wickramarathna <mastersilicon@gmail.com>
2023-05-02 00:03:04 +0530

    Added documentation strings and API related initial files

---
+++ RickAndMorty/APIClient/RMEndpoint.swift
@@ -0,0 +1,15 @@
+//
+//  RMEndpoint.swift
+//  RickAndMorty
+//
+//  Created by Nalinda Wickramarathna on 2023-05-01.
+//
+
+import Foundation
+
+/// Represent unique API endpoint
+@frozen enum RMEndpoint: String {
+    case character
+    case location
+    case episode
+}
---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Nalinda Wickramarathna on 2023-05-01.
+//
+
+import Foundation
+
+/// Object that represent a single API call
+final class RMRequest {
+    
+}
