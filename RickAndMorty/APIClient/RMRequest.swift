2faf2b10a87a5e42c4375644c0729dbb8471f27d
Nalinda Wickramarathna <mastersilicon@gmail.com>
2023-05-04 18:48:52 +0530

    RMRequest class implemented

--- RickAndMorty/APIClient/RMRequest.swift
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -9,5 +9,46 @@ import Foundation
 
 /// Object that represent a single API call
 final class RMRequest {
+    private struct Constants {
+        static let baseUrl = "https://rickandmortyapi.com/api/"
+    }
     
+    private let endpoint: RMEndpoint
+    
+    private let pathComponents: [String]
+    
+    private let queryParameters: [URLQueryItem]
+
+    private var urlString: String {
+        var string = Constants.baseUrl
+        string += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            pathComponents.forEach({
+                string += "/\($0)"
+            })
+        }
+        
+        if !queryParameters.isEmpty {
+            string += "?"
+            let argumentString = queryParameters.compactMap({
+                guard let value = $0.value else { return nil }
+                return "\($0.name)=\(value)"
+            }).joined(separator: "&")
+            
+            string += argumentString
+        }
+        
+        return string
+    }
+    
+    public var url: URL? {
+        return URL(string: urlString)
+    }
+    
+    public init(endpoint: RMEndpoint, pathComponents: [String] = [], queryParameters: [URLQueryItem] = []) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.queryParameters = queryParameters
+    }
 }
