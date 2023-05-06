e501429cef0931f8339a513e0f41454fd85a6f08
Nalinda Wickramarathna <mastersilicon@gmail.com>
2023-05-07 00:37:49 +0530

    Character cell basic implemented with its viewmodel class

---
+++ RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  RMCharacterCollectionViewCellViewModel.swift
+//  RickAndMorty
+//
+//  Created by Nalinda Wickramarathna on 2023-05-06.
+//
+
+import Foundation
+
+final class RMCharacterCollectionViewCellViewModel {
+    
+    public let characterName: String
+    private let characterStatus: RMCharacterStatus
+    private let characterImageUrl: URL?
+    
+    //MARK: - Init
+    
+    init(
+        characterName: String,
+        characterStatus: RMCharacterStatus,
+        characterImageUrl: URL?
+    ) {
+        self.characterName = characterName
+        self.characterStatus = characterStatus
+        self.characterImageUrl = characterImageUrl
+    }
+    
+    public var characterStatusText: String {
+        return characterStatus.rawValue
+    }
+    
+    public func fetchImage(completion: @escaping (Result<Data, Error>) -> Void) {
+        guard let url = characterImageUrl else {
+            completion(.failure(URLError(.badURL)))
+            return
+        }
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                completion(.failure(URLError(.badServerResponse)))
+                return
+            }
+            completion(.success(data))
+        }
+        task.resume()
+    }
+}
